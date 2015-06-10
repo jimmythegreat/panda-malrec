f260cda1a34a8b3b7be138f614da8dd2776a2737
Jimmy TheGreat <jimmytgreat@gmail.com>
2015-06-10 13:14:56 -0500

    These files are no longer used

--- scripts/movies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-BASEDIR=/home/brendan/malrec
-sqlite3 ${BASEDIR}/db/panda.db 'SELECT uuid FROM samples' | tail -n 100 | parallel -u -j 8 sh ${BASEDIR}/scripts/mkmovie.sh {/}
--- scripts/pack.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "${1}"
-~/git/panda/scripts/rrpack.py "${1}"
-rm "${1}"-rr-snp "${1}"-rr-nondet.log
